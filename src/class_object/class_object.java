608a512f8197a6cb9d231c00434eb8e48774b15b
Md. Riadul Islam <riadnwu@gmail.com>
2020-08-21 14:20:09 +0600

    Initial commit

---
+++ src/class_object/class_object.java
@@ -0,0 +1,27 @@
+package class_object;
+
+import java.util.Scanner;
+
+public class class_object {
+
+    public static void main(String a[])
+    {
+        int x;
+
+        Scanner input=new Scanner(System.in);
+        x=input.nextInt();
+
+        switch(x)
+        {
+            case 50:
+                break;
+            case 90:
+                break;
+            default:
+        }
+
+
+
+
+    }
+}
---
+++ src/class_object/new_class.java
@@ -0,0 +1,15 @@
+package class_object;
+
+public class new_class {
+    private int x;
+
+    public void setX(int a)
+    {
+        x=a;
+    }
+    public int getX()
+    {
+        return x;
+    }
+
+}
